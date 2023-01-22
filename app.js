17d2181b07e8867f8f2d1321835bdb4e98c164a1
Jo-nathan-grey <Jo.nathan.grey@gmail.com>
2023-01-22 21:51:21 +0700

    grid

--- app.js
+++ app.js
@@ -28,3 +28,25 @@ links.forEach((link) => {
     forms.classList.toggle("showSignup");
   });
 });
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.lbimg')
+images.forEach (image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  // if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
